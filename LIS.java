2600aaf896a83b4bbdd93b46edd1bfe956289441
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2022-08-27 09:41:17 -0500

    Update LIS.java

--- LIS.java
+++ LIS.java
@@ -84,3 +84,63 @@ class Solution {
         return low;
     }
 }
+
+//number of lis
+// 673.
+// time - O(n^2)
+// space - O(n)
+
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        //edge
+        if(nums == null || nums.length == 0)
+        {
+            return 0;
+        }
+        
+        int[] lis = new int[nums.length]; //lis[i] = length of lis with nums[i] as last element 
+        int[] countLis = new int[nums.length]; //countLis[i] = number of lis with length = lis[i] and nums[i] as last element
+        
+        lis[0] = 1; //if only 0th element is present, list[0] = 1
+        countLis[0] = 1; //number of lis of length 1 with nums[0] as last element nums[0] = 1
+        
+        int lisLength = 1; //return val
+        
+        for(int i = 1; i < nums.length; i++)
+        {
+            lis[i] = 1; //initial values considering nums[i] as seperate subsequence on its own
+            countLis[i] = 1; 
+            for(int j = 0; j < i; j++)
+            {
+                if(nums[i] > nums[j])
+                {
+                    //nums[i] can attach to lis ending with nums[j]
+                    if(1 + lis[j] > lis[i])
+                    {
+                        lis[i] = 1 + lis[j]; //update lis
+                        countLis[i] = countLis[j]; //number of lis ending with nums[i] is same as that ending with nums[j]
+                    }
+                    else if(1 + lis[j] == lis[i])
+                    {
+                        //lis[i] stays same, but number of lis with last element as nums[i] becomes,
+                        //current value + number of lis with nums[j] as last element
+                        countLis[i] += countLis[j]; 
+                    }
+                    lisLength = Math.max(lisLength, lis[i]);
+                }
+            }
+        }
+        
+        int result = 0; //return value
+        //find all countLis[i] with lis[i] = lisLength
+        for(int i = 0 ; i < nums.length; i++)
+        {
+            if(lis[i] == lisLength)
+            {
+                result += countLis[i];
+            }
+        }
+        
+        return result;
+    }
+}
