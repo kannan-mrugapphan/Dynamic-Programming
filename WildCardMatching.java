76a4743e5d56ee566c58496aae3aa04a98a610e0
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2023-07-04 20:11:56 -0500

    Update WildCardMatching.java

--- WildCardMatching.java
+++ WildCardMatching.java
@@ -1,117 +1,123 @@
 // 44.
 class Solution {
     public boolean isMatch(String s, String p) {
-        //edge
-        if(s.equals(p) || p.equals("*"))
-        {
-            return true; //both source and pattern are same
-        }
-        if(s.length() == 0 || p.length() == 0)
-        {
-            return false; //one of the strings is empty
-        }
-        
-        return wildcardMatchTwoPointers(s, p);
+        //return isValid(p, s, p.length(), s.length(), new Boolean[p.length() + 1][s.length() + 1]);
+        return isValid(p, s);
     }
-    
-    //time - O(length of source * length of pattern)
-    //space - O(length of source * length of pattern)
-    private boolean wildcardMatchDP(String s, String p) {
-        int sLen = s.length();
-        int pLen = p.length();
-        boolean[][] result = new boolean[sLen + 1][pLen + 1]; //source along rows and pattern along cols
-        
-        //base
-        result[0][0] = true; //empty pattern matches empty source
-        for(int i = 1; i <= sLen; i++)
+
+    //isValid(i, j) tracks if pattern[0, i] matches word[0, j]
+    //time - O(2^(m+n))
+    //space - O(m+n)
+    //time with memoization - O(mn)
+    //space with memoization - O(mn) for cache and O(m+n) for call stack
+    private boolean isValid(String pattern, String word, int i, int j, Boolean[][] cache)
+    {
+        //base 1
+        if(j == 0)
         {
-            result[i][0] = false; //any source cant match an empty pattern
-        }
-        for(int i = 1; i <= pLen; i++)
-        {
-            if(p.charAt(i - 1) == '*') //exploring the 0 case alone i.e not including * in pattern and matching other parts
+            //all chars in word are matched
+            while(i > 0)
             {
-                result[0][i] = result[0][i - 1]; 
-            }
-            //all other cells in 0th row are false -> source is empty
-        }
-        
-        for(int i = 1; i <= sLen; i++)
-        {
-            for(int j = 1; j <= pLen; j++)
-            {
-                if(s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?')
+                if(pattern.charAt(i - 1) == '*')
                 {
-                    //current char matches -> so match s[0 to i - 1] and p[o to j - 1]
+                    i--; //current char in pattern is * so skip it and not match with any char in word
+                }
+                else
+                {
+                    return false; //valid y=unmatched char is present in pattern that isn't matched with word
+                }
+            }
+
+            return true; //all chars in word are matched
+        }
+        //base 2
+        if(i == 0)
+        {
+            //pattern exhausted but word has more chars
+            return false;
+        }
+
+        //check cache
+        if(cache[i][j] != null)
+        {
+            return cache[i][j];
+        }
+
+        //if current character matches or current char in pattern is ? which matches any char in word
+        if(pattern.charAt(i - 1) == word.charAt(j - 1) || pattern.charAt(i - 1) == '?')
+        {
+            boolean result = isValid(pattern, word, i - 1, j - 1, cache);
+            cache[i][j] = result; //update cache
+            return result;
+        }
+
+        //else if current current char in pattern is * which matches 0 or more chars in word
+        else if(pattern.charAt(i - 1) == '*')
+        {
+            boolean skip = isValid(pattern, word, i - 1, j, cache); //skip current * to account for * not matching any char
+            boolean pick = isValid(pattern, word, i, j - 1, cache); //current * matches j in word and i remains at current * to account for it matching multiple chars in word
+
+            boolean result = skip || pick; //return true if one case returns true
+            cache[i][j] = result; //update cache
+            return result;
+        }
+
+        cache[i][j] = false; //update cache
+        return false; //else case char at pattern is not ? or * and not macthes with char in word
+    }
+
+    //time - O(mn)
+    //space - O(mn) 
+    private boolean isValid(String pattern, String word)
+    {
+        //result[i][j] tracks if pattern[0, i] matches word[0, j]
+        boolean[][] result = new boolean[pattern.length() + 1][word.length() + 1];
+
+        result[0][0] = true; //emptry pattern matches empty word - base case
+        for(int i = 1; i <= word.length(); i++)
+        {
+            result[0][i] = false; //empty pattern doesn't match non-empty word - base case
+        }
+
+        for(int i = 1; i <= pattern.length(); i++)
+        {
+            if(pattern.charAt(i - 1) == '*')
+            {
+                result[i][0] = result[i - 1][0]; //empty word and * in pattern - skip case - base case
+            }
+            else
+            {
+                result[i][0] = false; //non empty pattern empty word - base case
+            }
+        }
+
+        for(int i = 1; i <= pattern.length(); i++)
+        {
+            for(int j = 1; j <= word.length(); j++)
+            {
+                //if current character matches or current char in pattern is ? which matches any char in word
+                if(pattern.charAt(i - 1) == word.charAt(j - 1) || pattern.charAt(i - 1) == '?')
+                {
                     result[i][j] = result[i - 1][j - 1];
                 }
-                else if(p.charAt(j - 1) == '*')
+
+                //else if current current char in pattern is * which matches 0 or more chars in word
+                else if(pattern.charAt(i - 1) == '*')
                 {
-                    boolean zeroCase = result[i][j - 1]; //ignore current * and match s[o to i] and p[o to j - 1]
-                    boolean oneCase = result[i - 1][j]; //consider that current * matches matches current char at s and match s[0 to i - 1] and p[0 t o j]
-                    result[i][j] = zeroCase || oneCase;
+                    boolean skip = result[i - 1][j]; //skip current * to account for * not matching any char
+                    boolean pick = result[i][j - 1]; //current * matches j in word and i remains at current * to account for it matching multiple chars in word
+
+                    result[i][j] = skip || pick; //return true if one case returns true
                 }
-                else //no need to set explicitly
+
+                //else case char at pattern is not ? or * and not macthes with char in word
+                else
                 {
-                    //chars not equal
                     result[i][j] = false;
                 }
             }
         }
-        
-        return result[sLen][pLen];
-    }
-    
-    //time - O(length of source + length of pattern)
-    //space - O(length of source + length of pattern)
-    private boolean wildcardMatchTwoPointers(String s, String p) {
-        int sp = 0; //sp and pp iterates overs s and p
-        int pp = 0;
-        int s$ = -1; //s$ and p$ are used to pin the current location of sp and pp in case of * in p
-        int p$ = -1;
-        
-        while(sp < s.length()) //as long as source string is not explored
-        {
-            if(pp < p.length() && (s.charAt(sp) == p.charAt(pp) || p.charAt(pp) == '?'))
-            {
-                //pp within bounds and current char matches in both strings so advance 
-                sp++;
-                pp++;
-            }
-            else if(pp < p.length() && p.charAt(pp) == '*')
-            {
-                //pin the locations of sp and pp using s$ and p$
-                s$ = sp;
-                p$ = pp;
-                //advance pp - zero case where * doesnt match to any char in s
-                pp++;
-            }
-            else if(p$ == -1)
-            {
-                //actual char mismatch
-                return false;
-            }
-            else
-            {
-                //exploring zero case dint work - so return to pinned locations
-                sp = s$;
-                pp = p$;
-                //explore one case where the current * matches current char in s
-                sp++;
-            }
-        }
-        
-        //there could be a case whre s is exhausted and pp is at a point where all remaining chars in p are *
-        //in such case return true - zero case
-        while(pp < p.length())
-        {
-            if(p.charAt(pp) != '*')
-            {
-                return false;
-            }
-            pp++;
-        }
-        
-        return true;
+
+        return result[pattern.length()][word.length()];
     }
 }
