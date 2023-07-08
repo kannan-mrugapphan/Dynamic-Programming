e881aed6ea2e9e141ed0f17f3766565a0ad02045
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2023-07-08 13:14:54 -0500

    Update PalindromePartitioning2.java

--- PalindromePartitioning2.java
+++ PalindromePartitioning2.java
@@ -1,50 +1,95 @@
 // 132.
-// time - O(n^2) for building the boolean matrix and O(n^2) for finding min cuts
-// space - O(n^2)
 class Solution {
     public int minCut(String s) {
-        //edge
-        if(s == null || s.length() == 0)
+        //eg: s = 'abc'
+        //only possible partition = 'a', 'b', 'c'
+        //formed by 1 + f(bc) -> 1 + f(c) -> 1 + f('') 
+        //            3       <-   2      <-   1      <- 0
+        //even after string exhausted, it considers the last partition palindromeSubstring + f('') as a partition
+        //last valid partition is not needed
+        //return findMinPartitions(s, 0, new Integer[s.length()]) - 1;
+        return findMinPartitions(s) - 1;
+    }
+
+    //input string can be broken into n-1 strings where each string is one character which is a palindrome
+    //max partitions needed is n-1 -> minimize the number of partitions
+
+    //findMinPartitions(i) returns min partitions to break s[i, length - 1]
+    //space - O(n) max call stack size where each character has its own partition to form n-1 partitions
+    //time with memoization - O(n ^ 2) -> n states and for loop for each state
+    //space with memoization - O(n) for cache and O(n) for call stack
+    private int findMinPartitions(String s, int start, Integer[] cache)
+    {
+        //base
+        if(start == s.length())
         {
-            return 0; //no cuts needed
+            return 0; //string exhausted, no more partitions possible
         }
-        
-        //tracks if every substring in s is a palindrome
-        boolean[][] isSubstringPalindrome = new boolean[s.length()][s.length()]; 
-        
-        //for all i starting from last index
-        //for each i consider all j starting from i and going till end of string
-        for(int i = s.length() - 1; i >= 0; i--)
+
+        //chec cache
+        if(cache[start] != null)
         {
-            for(int j = i; j < s.length(); j++)
+            return cache[start];
+        }
+
+        int minPartitions = Integer.MAX_VALUE; //return value
+        for(int index = start; index < s.length(); index++)
+        {
+            String currentSubstring = s.substring(start, index + 1); //substing last argument is exclusive
+            //break s[start, index] into 1 partition
+            if(isPalindrome(currentSubstring))
             {
-                //consider the substring s(i, j)
-                //if the char at i and j are same and the size of substring less than 4 or if s(i + 1, j - 1)
-                //is a plaindrome then s(i, j) is a palindrome
-                if(s.charAt(i) == s.charAt(j) && (j - i <= 2 || isSubstringPalindrome[i + 1][j - 1]))
-                {
-                    isSubstringPalindrome[i][j] = true;
-                }
+                int partitions = 1 + findMinPartitions(s, index + 1, cache); //recursively break remaining string
+                minPartitions = Math.min(minPartitions, partitions); //update result
             }
         }
         
-        int[] cutsNeeded = new int[s.length()]; //ith index tracks min cuts needed to break a string of length i
-        
-        for(int i = 1; i < s.length(); i++)
+        cache[start] = minPartitions; //update cache
+        return minPartitions;
+    }
+
+    //time - O(n^2)
+    //space - O(n)
+    private int findMinPartitions(String s)
+    {
+        //result[i] tracks min partitions to break s[i, end of string]
+        int[] result = new int[s.length() + 1];
+        Arrays.fill(result, Integer.MAX_VALUE); //minimize result
+        result[s.length()] = 0; //empty string - base case
+
+        for(int start = s.length() - 1; start >= 0; start--)
         {
-            int maxCuts = i; //max cuts needed for a string of size i is i
-            for(int j = 0; j <= i; j++)
+            for(int index = start; index < s.length(); index++)
             {
-                if(isSubstringPalindrome[j][i])
+                String currentSubstring = s.substring(start, index + 1); //substing last argument is exclusive
+                //break s[start, index] into 1 partition
+                if(isPalindrome(currentSubstring))
                 {
-                    //if s(j, i) is a palindrome
-                    int currentCutsNeeded = (j == 0) ? 0 : 1 + cutsNeeded[j - 1];
-                    maxCuts = Math.min(maxCuts, currentCutsNeeded); //minmizing max cuts needed
+                    int partitions = 1 + result[index + 1]; //recursively break remaining string
+                    result[start] = Math.min(result[start], partitions); //update result
                 }
             }
-            cutsNeeded[i] = maxCuts;
         }
-        
-        return cutsNeeded[cutsNeeded.length - 1];
+
+        return result[0];
+    }
+
+    private boolean isPalindrome(String s)
+    {
+        int start = 0;
+        int end = s.length() - 1;
+
+        //as long as there are valid chars
+        while(start <= end)
+        {
+            if(s.charAt(start) != s.charAt(end))
+            {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
     }
 }
